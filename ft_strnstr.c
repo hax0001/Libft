ba1c224b833852b061d1f4d93ea40c7799674011
hax TC <hax@ubunto.myguest.virtualbox.org>
2023-11-03 19:54:45 +0100

    jm3a

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,26 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hax <hax@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 19:41:02 by nait-bou          #+#    #+#             */
-/*   Updated: 2023/11/02 22:07:01 by nait-bou         ###   ########.fr       */
+/*   Updated: 2023/11/03 19:43:16 by hax              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdd
+#include <stddef.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	int	i;
+	int	j;
 
+	j = 0;
 	i = 0;
 	if (*little == '\0')
 		return ((char *)big);
-	while (big[i] != '\0' && little[i] != '\0' && len != 0 && i < (int)len)
+	while (big[i] != '\0' && len != 0 && i < (int)len)
 	{
-		if (big[i] == little[i])
-			return ((char)little[i]);
+		if (big[i] == little[j])
+		{
+			while (big[i + j] == little[j] && i + j < (int)len)
+			{
+				if (little[j + 1] == '\0')
+					return ((char *)big + i);
+				j++;
+			}
+			j = 0;
+		}
 		i++;
 	}
 	return (NULL);
