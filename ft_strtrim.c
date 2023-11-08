7949f36247732ad7d5dacc6ad36a3e8049223c65
Nour Eddine Ait Bouguarri <nait-bou@c3r10p1.1337.ma>
2023-11-08 20:11:43 +0100

    wa srbi

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,40 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 14:21:37 by nait-bou          #+#    #+#             */
-/*   Updated: 2023/11/08 09:25:24 by nait-bou         ###   ########.fr       */
+/*   Created: 2023/11/08 09:49:06 by nait-bou          #+#    #+#             */
+/*   Updated: 2023/11/08 13:52:14 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
-	int		t;
-	int		n;
-	char	*d;
+	int		j;
+	char	*p;
 
-	i = 0;
-	t = 0;
-	n = 0;
-	while (s1[i++])
-		n++;
-	i = 0;
-	while (s2[i++])
-		n++;
-	i = 0;
-	d = malloc(n);
-	if (!d)
-		return (NULL);
-	while (s1[i] != '\0' && t < (n))
-		d[t++] = s1[i++];
-	i = 0;
-	while (s2[i] != '\0' && t < (n))
-		d[t++] = s2[i++];
-	return (d);
+	p = NULL;
+	if (s1 != 0 && set != 0)
+	{
+		i = 0;
+		j = strlen(s1);
+		while (s1[i] && strchr(set, s1[i]))
+			i++;
+		while (s1[j - 1] && strchr(set, s1[j - 1]) && j > i)
+			j--;
+		p = malloc(j - i + 1);
+		if (p)
+			strncpy(p, &s1[i], j - i + 1);
+	}
+	return (p);
 }
