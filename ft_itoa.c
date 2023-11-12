e530e11f8cf478d3fca4210c54ab61421226a470
Nour Eddine Ait Bouguarri <nait-bou@c3r4p6.1337.ma>
2023-11-12 23:28:58 +0100

    k

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,20 +6,23 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:57:19 by nait-bou          #+#    #+#             */
-/*   Updated: 2023/11/12 22:07:27 by nait-bou         ###   ########.fr       */
+/*   Updated: 2023/11/12 23:01:19 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
 int	nlen(int n)
 {
 	int	len;
 
-	int = 0;
+	int len = 0;
 	while(n != 0)
 	{
-		n %= 10
+		n /= 10;
+		len++;
 	}
+	return (len);
 }
 
 char	*ft_itoa(int n)
@@ -28,4 +31,5 @@ char	*ft_itoa(int n)
 	unsigned int	len;
 	
 	len = nlen(n);
+	s = (char *)malloc(sizeof(char) * len + 1)
 }
