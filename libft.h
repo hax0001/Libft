281e30cdb0030f2aaef70dd0771cf3851e01c1c6
Nour Eddine Ait Bouguarri <nait-bou@c4r7p7.1337.ma>
2023-11-27 23:17:56 +0100

    a2

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 18:05:14 by prossi            #+#    #+#             */
-/*   Updated: 2023/11/27 22:08:44 by nait-bou         ###   ########.fr       */
+/*   Updated: 2023/11/27 23:15:46 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
+# include <stdint.h>
 
 typedef struct s_list
 {
