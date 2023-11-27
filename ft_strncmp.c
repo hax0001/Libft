800553912d86640ab43d394ee27498450cf8889b
Nour Eddine Ait Bouguarri <nait-bou@e1r13p1.1337.ma>
2023-11-27 03:21:11 +0100

    gogogo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:47:41 by nait-bou          #+#    #+#             */
-/*   Updated: 2023/11/27 02:52:37 by nait-bou         ###   ########.fr       */
+/*   Updated: 2023/11/27 03:18:33 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (*s1 != '\0' && *s1 == *s2 && n > 0)
 	{
